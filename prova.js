004f172e30263d928d2eed1e047aa1ef7bd33ca1
Ardison Krasniqi <a.krasniqi@donboscosandona.it>
2024-10-25 13:13:31 +0200

    gg

---
+++ prova.js
@@ -0,0 +1,14 @@
+let nome = "Mengo";
+
+let sesso = "f";
+
+let frase;
+
+if(sesso == "m"){
+   frase = "Benvenuto " + nome
+}
+else{
+    frase = "Benvenuta " + nome
+}
+
+console.log(frase)
---
+++ teoremadipitagora/teoremdipitagora.js
@@ -0,0 +1,7 @@
+let a = 6
+let b = 5
+
+let risultato;
+
+risultato = (sqrt(a*a)+sqrt(b*b))
+console.log(risultato)
