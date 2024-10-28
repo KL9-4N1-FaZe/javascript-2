d93f624639f29001992f2826628f6397f4b3de82
Ardison Krasniqi <a.krasniqi@donboscosandona.it>
2024-10-28 09:52:53 +0100

    fatto semaforo

---
+++ semaforo.js
@@ -0,0 +1,17 @@
+let nome = "Il semaforo è";
+
+let colore = "v";
+
+if  (colore == "v") {
+    frase = nome + " verde";
+}
+
+else if (colore == "r") {
+    frase = nome + " rosso";
+}
+
+else if (colore == "g") {
+    frase = nome + " giallo";
+}
+
+console.log(frase);
--- teoremadipitagora.js
+++ teoremadipitagora.js
@@ -4,10 +4,10 @@ let b = 5;
 let quadrato1 = a ** 2;
 let quadrato2 = b ** 2;
 
-let somma = quadrato1 * quadrato2;
+let somma = quadrato1 + quadrato2;
 
 let ipotenusa = Math.sqrt(somma);
 
 let risultato = ipotenusa.toFixed(2);
 
-console.log(ipotenusa);
+console.log(risultato);
