606f88da51d22595c7e81468b19f50944058bf08
Ardison Krasniqi <a.krasniqi@donboscosandona.it>
2024-11-25 10:55:01 +0100

    finisch

--- Fasceetà.js
+++ Fasceetà.js
@@ -2,28 +2,28 @@ for (let i = 0; i <= 100; i++) {
     let fascia = ""; 
 
     switch (true) {
-        case (i >= 0 && i <= 1):
+        case (i <= 1):
             fascia = "lattante";
             break;
-        case (i === 2):
+        case (i == 2):
             fascia = "prima infanzia";
             break;
-        case (i >= 3 && i <= 6):
+        case ( i <= 6):
             fascia = "seconda infanzia";
             break;
-        case (i >= 7 && i <= 22):
+        case (  i <= 22):
             fascia = "pubertà e adolescenza";
             break;
-        case (i >= 23 && i <= 39):
+        case ( i <= 39):
             fascia = "prima età adulta";
             break;
-        case (i >= 40 && i <= 59):
+        case (  i <= 59):
             fascia = "seconda età adulta";
             break;
-        case (i >= 60 && i <= 75):
+        case (  i <= 75):
             fascia = "terza età";
             break;
-        case (i >= 76 && i <= 90):
+        case (  i <= 90):
             fascia = "quarta età";
             break;
         case (i >= 91):
