d1896a6672289ab40ab0e212dc0cdab542f9f947
Ardison Krasniqi <a.krasniqi@donboscosandona.it>
2024-11-25 10:36:11 +0100

    rifinito

---
+++ negativiepositivi.js
@@ -0,0 +1,12 @@
+
+for( let i=-10; i<=10; i++) 
+{
+
+if(i < 0){
+    numero = i + " negativo ";
+}
+else {
+    numero = i + " positivo ";
+}
+console.log(numero);
+}
--- primociclofor.js
+++ primociclofor.js
@@ -2,4 +2,5 @@
 
 for( let i=10; i>=0; i--) {
     console.log(i);
+    
 }
