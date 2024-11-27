16b8f4cd6722338bb51b5cb6d445d30553efc861
Ardison Krasniqi <a.krasniqi@donboscosandona.it>
2024-11-27 13:15:18 +0100

    quasi finito

--- cerca.js
+++ cerca.js
@@ -2,3 +2,7 @@ let parole = ["albero", "casa", "computer", "auto", "mare", "giardino", "lampada
     "telefono", "computer", "bicicletta", "libro", "penna", "palla", "carro", "finestra", "cielo", "montagna"];
 
 let paroleDaCercare = ["auto", "scuola", "lampada", "computer", "bicicletta"];
+
+for(let i=0; i<paroleDaCercare.length; i++){
+   console.log(parole.indexOf(paroleDaCercare[i])) 
+}
