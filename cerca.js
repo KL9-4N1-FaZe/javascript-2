81c3c5a3cd0ed8608810be7bbe994bfd3da09334
Ardison Krasniqi <a.krasniqi@donboscosandona.it>
2024-11-27 13:01:50 +0100

    nomi pari finito

---
+++ cerca.js
@@ -0,0 +1,4 @@
+let parole = ["albero", "casa", "computer", "auto", "mare", "giardino", "lampada", "scuola", "sedia", "tavolo", 
+    "telefono", "computer", "bicicletta", "libro", "penna", "palla", "carro", "finestra", "cielo", "montagna"];
+
+let paroleDaCercare = ["auto", "scuola", "lampada", "computer", "bicicletta"];
---
+++ nomipari.js
@@ -0,0 +1,5 @@
+let nomi = ["Anna", "Marco", "Giulia", "Luca", "Francesca", "Giovanni", "Elena", "Alessandro", "Matteo", "Laura"];
+
+for (let i=1; i<=nomi.length; i=i+2){
+    console.log(nomi[i]); 
+}
